691e58d7a22c352200539a37e7fa9821cfa5a2ed
UriOriKay <127621006+UriOriKay@users.noreply.github.com>
2024-09-22 10:25:55 +0200

    login creates

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+
+  login() {
+    return true
+  }
+
+}
